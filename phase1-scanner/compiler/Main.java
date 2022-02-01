aad8acfc5eda02d2269c73d171f8a34ff0dc74b9
kimiasedighi <kimiasedighi2000@gmail.com>
2022-02-01 14:34:57 +0330

    fix constants convert to string AST

--- phase1-scanner/compiler/Main.java
+++ phase1-scanner/compiler/Main.java
@@ -77,10 +77,6 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-//        File file1 = new File("C:\\Users\\dell\\IdeaProjects\\compiler-project\\phase1-scanner\\tests\\t13-macro1.in");
-//        String string1=Main.run(file1);
-//        File file2 = new File("C:\\Users\\dell\\IdeaProjects\\compiler-project\\phase1-scanner\\tests\\t14-macro2.in");
-//        String string2=Main.run(file2);
         /* dshuisdhuihdsu/*fuihfuahuhd */
         File file3 = new File("D:\\University-term7\\compiler-project\\phase1-scanner\\compiler\\test.txt");
         boolean result=Main.run(file3);
