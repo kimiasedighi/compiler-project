a9bf988bb4497458c41f802dcbe51c6686597fd5
Reza <Reza.kalhor19@gmail.com>
2022-02-04 00:06:30 +0330

    adding tests

--- phase1-scanner/compiler/Test.java
+++ phase1-scanner/compiler/Test.java
@@ -10,8 +10,7 @@ public class Test {
 //        File file2 = new File("C:\\Users\\dell\\IdeaProjects\\compiler-project\\phase1-scanner\\tests\\t14-macro2.in");
 //        String string2=Main.run(file2);
         /* dshuisdhuihdsu/*fuihfuahuhd */
-        File file3 = new File("C:\\Users\\dell\\IdeaProjects\\compiler-project\\phase1-scanner\\compiler\\test.txt");
-        boolean result=Main.run(file3);
-
+        File file3 = new File("C:\\Users\\dell\\IdeaProjects\\compiler-project\\phase1-scanner\\tests\\t292-lib.d");
+        boolean result= main.run(file3);
     }
 }
